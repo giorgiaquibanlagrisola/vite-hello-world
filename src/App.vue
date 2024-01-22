31a049168d7904224f648c0f3c30f2db3b2521fc
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-01-22 17:06:30 +0100

    La mia prima app con Vite!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup>
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  data() {
+    return { 
+        count: 0
+     }
+  },
+  components: {
+    AppHeader
+  }
+}
 </script>
 
 <template>
+    <div>
+        <AppHeader />
+    </div>
 </template>
 
 <style scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+</script>
+
+<template>
+    <header>
+        <h1>
+            La mia prima app con Vite!
+        </h1>
+    </header>
+    
+</template>
+
+<style scoped>
+    header {
+        padding: 20px;
+        background-color: aqua;
+    }
+    h1 {
+        text-align: center;
+        color: blue;
+    }
+</style>
